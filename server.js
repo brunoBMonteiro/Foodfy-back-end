8c6e87b5a66be649aec07b35f25909646bda5fc5
Bruno Bilheri <52968377+brunoBMonteiro@users.noreply.github.com>
2020-11-25 11:26:43 -0300

    servidor reiniciando automaticamente com nodemon

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const server = express()
 
+server.get("/", function(res, res){
+     return res.send('Hi! How is going')
+})
+
 server.listen(5000, function() {
     console.log('server is running')
 } )
